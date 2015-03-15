89f6e64f39ea80bc76a4577cc4a12f4d4323f235
Erik Lindroos <gliptic@gmail.com>
2015-03-15 23:21:00 +0100

    Reducible signals, faster array traversal, made normal functions work as reducer-functions

--- src/transducers.ts
+++ src/transducers.ts
@@ -13,7 +13,8 @@ declare module "transducers" {
 }
 
 interface Signal {
-	connect(r: Reducer);
+	then(r: (input, res) => any, init?: any): any;
+	send(v: any): boolean;
 }
 
 type IteratorResult = { value?: any; done?: boolean };
@@ -23,9 +24,9 @@ interface Iterator {
 }
 
 interface Reducer {
-	a: () => any; // initial
-	b: (v) => any; // result
-	c: (res, input) => any; // step
+	(input, res): any; // step
+	a?: () => any; // initial
+	b?: (v) => any; // result
 }
 
 type Transducer = (t: Reducer) => Reducer;
@@ -42,23 +43,14 @@ define(function () {
 		return x === void 0;
 	}
 
-	var arrayReducer = { a: () => [], b: id, c: (arr, v) => { arr.push(v); return arr; } };
-
-	var symbolExists = !isUndef(Symbol);
-	var protocolIterator = symbolExists ? Symbol.iterator : '@@iterator';
+	var arrayReducer: Reducer = defaultReducer({a: () => [], b: id}, (v, arr) => { arr.push(v); return arr; });
+	
+	var protocolIterator = Symbol ? Symbol.iterator : '@@iterator';
 
 	function iterator(coll): Iterator {
 		var iter = coll[protocolIterator];
 		if (iter) return iter.call(coll);
 		else if (coll.next) return coll;
-		else {
-			var index = 0;
-			return {
-				next: function (): IteratorResult {
-					return index < coll.length ? { value: coll[++index] } : { done: true };
-				}
-			};
-		}
 	}
 
 	function compose(...p: Transducer[]): Transducer;
@@ -74,9 +66,7 @@ define(function () {
 	}
 
 	function transduce(coll, xform: Transducer, reducer: Reducer, init?): any {
-		var r = xform(reducer);
-		if (isUndef(init)) init = r.a();
-		return reduce(coll, r, init);
+		return reduce(coll, xform(reducer), init);
 	}
 
 	function reduced(value) {
@@ -87,164 +77,169 @@ define(function () {
 		return x && x.__ == reduced;
 	}
 
-	function reduce(coll, reducer: Reducer, init) {
-		var iter = iterator(coll),
-		 	result = init;
+	function arrayBind(coll, f, res) {
+		var i = 0;
 			
-		for (;;) {
-			var val = iter.next();
-			if (val.done)
-				return reducer.b(result);
+		for (; i < coll.length; ++i) {
+			res = f(coll[i], res);
+			if (isReduced(res))
+				return res;
+		}
 
-			result = reducer.c(result, val.value);
+		return res;
+	}
 
-			if (isReduced(result)) {
-				return reducer.b(result.v);
+	function reduce(coll, reducer: Reducer, init?) {
+		var result;
+
+		if (isUndef(init)) init = reducer.a && reducer.a();
+
+		if (coll instanceof Array) {
+			result = arrayBind(coll, reducer, init);
+		} else if (coll.then) {
+			result = coll.then(reducer, init);
+		} else {
+			result = init;
+
+			var iter = iterator(coll), val;
+				
+			for (;val = iter.next(), !val.done;) {
+				result = reducer(val.value, result);
+				if (isReduced(result))
+					return result;
 			}
 		}
+
+		result = isReduced(result) ? result.v : result;
+		if (reducer.b) result = reducer.b(result);
+		return result;
 	}
 
 	function into<T>(to: T, coll: any, xform: Transducer): T {
 		return transduce(coll, xform, arrayReducer, to);
 	}
 
+	function defaultReducer(reducer, f) {
+		f.a = reducer.a;
+		f.b = reducer.b;
+		return f;
+	}
+
 	function map(f: (v: any) => any): Transducer {
-		return r => { return {
-			a: r.a,
-			b: r.b,
-			c: (res, input) => r.c(res, f(input))
-		}};
+		return reducer => {
+			return defaultReducer(reducer, (input, res) => reducer(f(input), res));
+		};
 	}
 
 	function filter(f: (v: any) => boolean): Transducer {
-		return reducer => { return {
-			a: reducer.a,
-			b: reducer.b,
-			c: (res, input) => f(input) ? reducer.c(res, input) : res
-		}};
+		return reducer => {
+			return defaultReducer(reducer, (input, res) => f(input) ? reducer(input, res) : res);
+		};
 	}
 
-	function take(n: number): Transducer {
+	function take(n: number)/*: Transducer*/ {
 		return reducer => {
 			var l = n;
-			return {
-				a: reducer.a,
-				b: reducer.b,
-				c: (res, input) => {
-					// TODO: It would be better if this was reduced on the last
-					// call like in the original. Find some neat way to do that.
-					return l-- > 0 ? reducer.c(res, input) : reduced(res);
-				}
-			}
+			return defaultReducer(reducer, (input, res) => {
+				// TODO: It would be better if this was reduced on the last
+				// call like in the original. Find some neat way to do that.
+				return l-- > 0 ? reducer(input, res) : reduced(res);
+			});
 		};
 	}
 
 	function takeWhile(f: (v: any) => boolean): Transducer {
 		return reducer => {
-			return {
-				a: reducer.a,
-				b: reducer.b,
-				c: (res, input) => {
-					return f(input) ? reducer.c(res, input) : reduced(res);
-				}
-			}
+			return defaultReducer(reducer, (input, res) => {
+				return f(input) ? reducer(input, res) : reduced(res);
+			});
 		};
 	}
 
 	function drop(n: number): Transducer {
 		return reducer => {
 			var l = n;
-			return {
-				a: reducer.a,
-				b: reducer.b,
-				c: (res, input) => {
-					return (l-- > 0) ? res : reducer.c(res, input);
-				}
-			}
+			return defaultReducer(reducer, (input, res) =>
+				(l-- > 0) ? res : reducer(input, res));
 		};
 	}
 
 	function dropWhile(f: (v: any) => boolean): Transducer {
 		return reducer => {
 			var f2: any = f;
-			return {
-				a: reducer.a,
-				b: reducer.b,
-				c: (res, input) => {
-					return (f2 && f2(input)) ? res : (f2 = 0, reducer.c(res, input));
-				}
-			}
+			return defaultReducer(reducer, (input, res) =>
+				(f2 && f2(input)) ? res : (f2 = 0, reducer(input, res)));
 		};
 	}
 
 	// Concatenate a sequence of collections into one sequence
 	function cat(reducer: Reducer): Reducer {
-		return {
-			a: reducer.a,
-			b: reducer.b,
-			c: (res, input) => {
-				var innerReducer = {
-					a: reducer.a,
-					b: id,
-					c: (res2, input2) => {
-						var val = reducer.c(res2, input2);
-						return isReduced(val) ? val.v : val;
-					}
-				}
+		return defaultReducer(reducer, (input, res) => {
+			var innerReducer = defaultReducer(reducer, (input2, res2) => {
+				var val = reducer(input2, res2);
+				return isReduced(val) ? val.v : val;
+			});
 
-				return reduce(input, innerReducer, res);
-			}
-		}
+			return reduce(input, innerReducer, res);
+		});
 	}
 
+	// Also called flatMap, >>=, bind and others.
 	function mapcat(f): Transducer {
 		return compose(map(f), cat);
 	}
 
-	function pusher(r: Reducer): (v: any) => boolean {
-		var result = r.a();
+	function pusher(r: Reducer, init: any): (v: any) => boolean {
+		var result = init;
 		return v => {
-			result = r.c(result, v);
+			result = r(v, result);
 			return !isReduced(result);
 		};
 	}
 
-	function wrap(f: (input, res?) => any, init?): Reducer {
-		return {
-			a: () => init,
-			b: id,
-			c: f
-		};
-	}
-
-	function counter(r: Reducer): Reducer {
+	function counter(reducer: Reducer): Reducer {
 		var c = 0;
-		return {
-			a: r.a,
-			b: id,
-			c: (res, input) => r.c(res, ++c)
-		};
+		return defaultReducer(reducer, (input, res) => reducer([input, ++c], res));
 	}
 
-	function every10Seconds(): Signal {
-		var reducers = [];
+	function everySecond(): Signal {
+		var sig = signal();
 
 		function set() {
 			setTimeout(() => {
-				reducers = into([], reducers, filter(e => e(1)));
-				// TODO: If reducers cannot grow, we can let the signal disappear here
-				if (reducers.length || s.then)
-					set();
+				if (sig.send(1)) set();
 			}, 1000);
 		}
 
-		var s = {
-			then: r => {
-				reducers.push(pusher(r));
-			}
+		set();
+
+		return sig;
+	}
+
+	function after(ms: number, v?: any): Signal {
+		var sig = signal();
+
+		setTimeout(() => {
+			sig.send(v);
+		}, ms);
+		
+		return sig;
+	}
+
+	function signal(): Signal {
+		var reducers = [];
+
+		var ev = (v) => {
+			reducers = into([], reducers, filter(e => e(v)));
+			return !(!reducers.length && !s.then);
 		};
 
-		set();
+		var s = {
+			then: (r, init) => {
+				reducers.push(pusher(r, init));
+			},
+			send: ev
+		};
 
 		return s;
 	}
@@ -261,9 +256,13 @@ define(function () {
 		dropWhile: dropWhile,
 		cat: cat,
 		mapcat: mapcat,
-		pusher: pusher,
-		every10Seconds: every10Seconds,
-		counter: counter,
-		wrap: wrap
+		//counter: counter,
+
+		// Signals
+		//pusher: pusher,
+		//everySecond: everySecond,
+		after: after,
+		//signal: signal,
+		
 	};
 })
