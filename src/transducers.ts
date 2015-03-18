2dfa5083315fc3b5c362b9fa780877490f31662e
Erik Lindroos <gliptic@gmail.com>
2015-03-18 22:35:33 +0100

    Mostly working async join

--- src/transducers.ts
+++ src/transducers.ts
@@ -14,6 +14,7 @@ declare module "transducers" {
 
 interface Signal {
 	(v: any): boolean;
+	done(): any;
 	then(r: (input, res) => any, init?: any): any;
 }
 
@@ -26,8 +27,9 @@ interface Iterator {
 interface Reducer {
 	(input, res): any; // step
 	a?: () => any; // initial
-	b?: (v) => any; // result
+	b?: (v) => void; // result
 	c?: (v) => any; // clone
+	d?: (v, diff) => void;
 }
 
 type Transducer = (t: Reducer) => Reducer;
@@ -35,6 +37,9 @@ type Transducer = (t: Reducer) => Reducer;
 declare var Symbol: any;
 
 define(function () {
+
+	/** @const */
+	var ENABLE_FLUENT = true;
 
 	function id(v) {
 		return v;
@@ -50,13 +55,16 @@ define(function () {
 	var arrayReducer: Reducer = defaultReducer(
 		{ a: () => [], b: nop },
 		(v, arr) => { arr.push(v); });
+
+	var objReducer: Reducer = defaultReducer(
+		{ a: () => {}, b: nop },
+		objMerge);
 	
 	var protocolIterator = Symbol ? Symbol.iterator : '@@iterator';
 
 	function iterator(coll): Iterator {
 		var iter = coll[protocolIterator];
 		if (iter) return iter.call(coll);
-		return coll; // Assume it is an iterator already
 	}
 
 	function compose(...p: Transducer[]): Transducer;
@@ -75,12 +83,45 @@ define(function () {
 		return reduce(coll, xform(reducer), init);
 	}
 
-	function arrayBind(coll, f, res) {
+	function arrayBind(coll, f, res?) {
+		
+		/*
 		var c = 0;
 		for (var i = 0; !c && i < coll.length; ++i) {
 			c = f(coll[i], res);
 		}
 		return c;
+		*/
+		return coll.some(v => <any>f(v, res)); // TODO: Measure performance, for vs. forEach vs. some
+	}
+
+	function objBind(coll, f, res?) {
+		/*
+		var c = 0;
+		arrayBind(Object.keys(coll), k => {
+			c = f([k, coll[k]], res);
+		});
+		return c;
+		*/
+		return Object.keys(coll).some(k => <any>f([k, coll[k]], res));
+		
+	}
+
+	function objMerge(src, dest) {
+		if (Array.isArray(src)) {
+			dest[src[0]] = src[1];
+		} else {
+			objBind(src, v => objMerge(v, dest));
+		}
+	}
+
+	function getReducer(v) {
+		if (Array.isArray(v))
+			return arrayReducer;
+		else if (typeof v == "object")
+			return objReducer;
+		else
+			return arrayReducer; // Default to array
 	}
 
 	function reduce(coll, reducer: Reducer, init?) {
@@ -89,25 +130,30 @@ define(function () {
 		return result;
 	}
 
-	function internalReduce(coll, reducer: Reducer, result?) {
-		var c = 0;
+	function internalReduce(coll, reducer: Reducer, result?, skipFinish?: boolean) {
+		var c = false;
 		if (Array.isArray(coll)) {
 			c = arrayBind(coll, reducer, result);
 		} else if (coll.then) {
 			c = coll.then(reducer, result);
 		} else {
 			var iter = iterator(coll), val;
-			for (;val = iter.next(), !(c || val.done);) {
-				c = reducer(val.value, result);
+			if (!iter) {
+				c = objBind(coll, reducer, result);
+			} else {
+				for (;val = iter.next(), !(c || val.done);) {
+					c = reducer(val.value, result);
+				}	
 			}
+			
 		}
 
-		if (reducer.b) reducer.b(result);
+		if (!skipFinish && reducer.b) reducer.b(result);
 		return c;
 	}
 
 	function into<T>(to: T, coll: any, xform: Transducer): T {
-		return transduce(coll, xform, arrayReducer, to);
+		return transduce(coll, xform, getReducer(to), to);
 	}
 
 	function defaultReducer(reducer, f: Reducer) {
@@ -174,28 +220,29 @@ define(function () {
 	}
 
 	// Concatenate a sequence of reducible objects into one sequence
-	function cat(reducer: Reducer): Reducer {
-		return defaultReducer(reducer, (input, res) => {
-			// Wrap reducer to make .b into the identity function
-			return internalReduce(input, (input2, res2) => reducer(input2, res2), res);
-		});
+	function cat(): Transducer {
+		return (reducer: Reducer) => {
+			return defaultReducer(reducer, (input, res) => {
+				return internalReduce(input, reducer, res, true);
+			});
+		};
 	}
 
 	// Also called flatMap, >>=, bind and others.
 	function mapcat(f): Transducer {
-		return compose(map(f), cat);
+		return compose(map(f), cat());
 	}
 
+/*
 	function bufferAll(r: Reducer) {
 		var buffer = [];
-		var f: Reducer = (input, res) => { buffer.push(input); console.log('Pushing', input); };
+		var f: Reducer = (input, res) => { buffer.push(input); };
 		f.a = r.a;
 		f.b = res => {
-			console.log('Passing', buffer);
 			r(buffer, res)
 		};
 		return f;
-	}
+	}*/
 
 	function process(r: Reducer, result?: any): (v: any) => boolean {
 		var c = false;
@@ -226,48 +273,147 @@ define(function () {
 
 		setTimeout(() => {
 			sig(v);
+			sig.done();
 		}, ms);
 		
 		return sig;
 	}
 
-	function signal(): Signal {
-		var reducers = [];
+	function join(): Transducer {
+		return (reducer: Reducer) => {
+
+			var r: Reducer = (input, res) => reducer(input, res.s);
+
+			r.a = function () {
+				return {
+					s: reducer.a && reducer.a(),
+					o: 1 // We count calling .b in outstanding
+				};
+			};
+
+			r.b = function (res) {
+				r.d(res, -1);
+			};
+
+			r.d = function (res, diff) {
+				(res.o += diff) || res(res.s);
+			}
+
+			return r;
+		};
+	}
+
+	function signal(persistent?: boolean): Signal {
+		var reducers = [],
+			values = [];
 
 		var ev: any = (v) => {
-			reducers = reducers.filter(e => e(v));
+			persistent && values.push(v);
+			reducers = reducers.filter(r => !r(v) || !r.b());
 			return !!reducers.length || !!ev.then;
 		};
 
-		ev.then = proc => {
-			reducers.push(proc);
+		ev.then = (reducer: Reducer, result) => {
+			reducer.d && reducer.d(result, 1);
+
+			var receiver: Reducer = function(input) {
+				return reducer(input, result);
+			}
+
+			receiver.b = function() {
+				reducer.d && reducer.d(result, -1);
+			}
+
+			reducers.push(receiver);
+		};
+
+		ev.done = () => {
+			reducers = reducers.filter(r => {
+				return r.b();
+			}); // NOTE: Assumes .b returns a false value
 		};
 
 		return ev;
 	}
 
-	return {
-		compose: compose,
-		transduce: transduce,
-		into: into,
+	var transducerFunctions = {
 		map: map,
 		filter: filter,
 		take: take,
 		takeWhile: takeWhile,
 		drop: drop,
 		dropWhile: dropWhile,
-		cat: cat,
 		mapcat: mapcat,
-		reducep: reducep,
-		reduce: reduce,
-		bufferAll: bufferAll,
-		//counter: counter,
-
-		// Signals
-		process: process,
-		//everySecond: everySecond,
-		after: after,
-		//signal: signal,
-		
+		cat: cat,
+		join: join,
 	};
+
+	var mod;
+
+	if (ENABLE_FLUENT) {
+		// Methods on transducers for chaining
+		//var transducerKeys = Object.keys(transducerFunctions);
+
+		// Mod works like the null transreducer
+		mod = id;
+		objBind(transducerFunctions, v => {
+			var innerF = v[1];
+			function fluentWrapper() {
+				var rhs = innerF.apply(null, arguments),
+					lhs = this;
+				var td = r => lhs(rhs(r));
+				objBind(transducerFunctions, v2 => { td[v2[0]] = mod[v2[0]] });
+				td.apply = function (from, to) { return into(to, from, this); };
+				return td;
+			}
+
+			mod[v[0]] = fluentWrapper;
+		});
+	} else {
+		mod = transducerFunctions;
+	}
+	/* else {
+		var fluentMethods = {};
+		transducerKeys.forEach(k => {
+			var innerF = transducerFunctions[k];
+
+			function fluentWrapper() {
+				var lhs = this.td;
+				var args = Array.prototype.slice.call(arguments);
+				var rhs = r => lhs(innerF.apply(null, args)(r));
+				return { coll: this.coll, td: rhs };
+			}
+
+			fluentMethods[k] = fluentWrapper;
+		});
+
+		mod = function (coll) {
+			var obj = { coll: coll, td: id };
+			transducerKeys.forEach(k => {
+				obj[k] = fluentMethods[k]
+			});
+		};
+	}*/
+
+	objMerge({
+			transduce: transduce,
+			into: into,
+			compose: compose,
+			
+			
+			arrayReducer: arrayReducer,
+			
+			reducep: reducep,
+			reduce: reduce,
+			//bufferAll: bufferAll,
+			//counter: counter,
+
+			// Signals
+			process: process,
+			//everySecond: everySecond,
+			after: after,
+			//signal: signal,
+		}, mod);
+
+	return mod;
 })
